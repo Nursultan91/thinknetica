1826f8b532a7de6484116023a2ac84ecb662e358
iamrealnurs <nurs567@yandex.ru>
2019-05-21 18:59:48 +0600

    Something Works

--- 4task/main.rb
+++ 4task/main.rb
@@ -3,19 +3,75 @@ require_relative 'station'
 require_relative 'train'
 require_relative 'route'
 
-wall = Station.new("Wall")
-wf = Station.new("Winterfell")
-hg = Station.new("Highgarden")
-kl = Station.new("Kings Landing")
-cr = Station.new("Casterly Rock")
-north = Route.new(wall, cr)
-sam = Train.new(1, "cargo", 4)
-john = Train.new(2, "cargo", 4)
-davos = Train.new(3, "cargo", 4)
-north.add_station(wf)
-north.add_station(hg)
-north.add_station(kl)
-sam.take_route(north)
-john.take_route(north)
-davos.take_route(north)
+class Main
+  attr_reader :stations, :routes, :trains
+
+  def initialize
+    @stations = []
+    @routes = []
+    @trains = []
+  end
+
+  def create_station
+    puts "Введите название станции"
+    name = gets.chomp
+    station = Station.new(name)
+    @stations << station
+  end
+
+  def create_route
+    puts "Введите номер первой станции выбрав ее из списка"
+    @stations.each_with_index {|station, index| puts "#{index} - #{station.title}"}
+    first_station_index = gets.to_i
+    puts "Введите номер последней станции выбрав ее из списка"
+    @stations.each_with_index {|station, index| puts "#{index} - #{station.title}"}
+    last_station_index = gets.to_i
+    route = Route.new(@stations[first_station_index], @stations[last_station_index])
+    @routes << route
+  end
+
+  def create_train
+    puts "Введите номер поезда"
+    number = gets.to_i
+    puts "Выберите тип: 1 - грузовой, 2 - пассажирский"
+    train_type_choice = gets.to_i
+    if train_type_choice == 1
+      type = "cargo"
+    elsif train_type_choice == 2
+      type = "pass"
+    else
+      puts "Нет такого типа. Попробуйте еще раз"
+    end
+    puts "Введите количество вагонов"
+    wagons = gets.to_i
+    train = Train.new(number, type, wagons)
+    @trains << train
+  end
+
+  def go
+    loop do
+      puts "Введите что вы хотите сделать?"
+      puts "1 - создать станцию"
+      puts "2 - создать маршрут"
+      puts "3 - создать поезд"
+      puts "quit - завершить программу"
+
+      choice = gets.chomp
+      break if choice == "quit"
+      case choice.to_i
+      when 1
+        create_station
+      when 2
+        create_route
+      when 3
+        create_train
+      else
+        puts "Я не знаю чего ты хочешь"
+      end
+    end
+  end
+end
+
+asoiaf = Main.new
+asoiaf.go
 pry
